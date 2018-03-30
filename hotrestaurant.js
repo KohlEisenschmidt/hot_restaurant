aa1ec11f512a3ceb534c9cdc1428f1291d043ff6
EllenStafford <ehs@ehstafford.com>
2018-03-30 09:33:28 -0400

    First commit

---
+++ hotrestaurant.js
@@ -0,0 +1,35 @@
+//Frontend
+//Create home page index.html (Bootstrap Jumbotron)
+    //View Tables button
+    //Make Reservations button
+    //API Table Link
+    //API Wait List Link
+//Create tables page tables.html
+    //View Tables button
+//Create reservations page res.html
+    //Labels: Name, Phone number, Email, Unique ID
+
+//Backend
+//Create API Links
+    //Create clear Table
+        //Clear-reset the form
+
+    //Create API Table link
+        //Create form
+            //customerName, input type VARCHAR(50)
+            //phoneNumber, input type INTEGER (10)
+            //customerEmail, input type VARCHAR(50)
+            //customerId, input type INTEGER(10)
+
+    //Create API Wait list
+            //Create form
+            //customerName, input type VARCHAR(50)
+            //phoneNumber, input type INTEGER (10)
+            //customerEmail, input type VARCHAR(50)
+            //customerId, input type INTEGER(10)
+
+    //Create dependencies
+        //require express
+        //require body-parser
+        //require path
+        //declare variable express and PORT 3000
